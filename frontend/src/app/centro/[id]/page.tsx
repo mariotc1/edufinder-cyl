8b356979c29845f062c13d09efb7c14662cacab6
mariotc1 <mariotomecore@gmail.com>
2026-01-26 22:48:20 +0100

    color de fondo unificado

--- frontend/src/app/centro/[id]/page.tsx
+++ frontend/src/app/centro/[id]/page.tsx
@@ -105,7 +105,7 @@ export default function CentroDetail() {
     };
 
     return (
-        <div className="min-h-screen bg-neutral-50">
+        <div className="min-h-screen bg-brand-gradient">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
                 {/* Breadcrumb */}
                 <Link 
