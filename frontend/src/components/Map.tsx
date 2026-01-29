0c73ce3c52040666aa4625da06e1ec1747c30d09
mariotc1 <mariotomecore@gmail.com>
2026-01-29 09:37:54 +0100

    mejora UI cards

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -192,8 +192,8 @@ export default function Map({ centros, userLocation, radius, center, zoom, favor
                          <Popup 
                             className="premium-popup-container" 
                             closeButton={false} 
-                            maxWidth={400}
-                            minWidth={400}
+                            maxWidth={500}
+                            minWidth={500}
                          >
                             <MapPopup centro={centro} initialIsFavorite={favoriteIds.includes(centro.id)} />
                          </Popup>
