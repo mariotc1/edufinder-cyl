fe313bd2d7b1c03e7efd481bd403291fa6a0d937
mariotc1 <mariotomecore@gmail.com>
2026-02-03 15:37:17 +0100

    clean code

--- backend/database/migrations/2026_01_29_191143_add_github_id_to_users_table.php
+++ backend/database/migrations/2026_01_29_191143_add_github_id_to_users_table.php
@@ -1,27 +1,20 @@
 <?php
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    return new class extends Migration {
 
-return new class extends Migration {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('github_id')->nullable()->after('google_id');
-        });
-    }
+        public function up(): void {
+            Schema::table('users', function (Blueprint $table) {
+                $table->string('github_id')->nullable()->after('google_id');
+            });
+        }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('github_id');
-        });
-    }
-};
+        public function down(): void {
+            Schema::table('users', function (Blueprint $table) {
+                $table->dropColumn('github_id');
+            });
+        }
+    };
+?>
