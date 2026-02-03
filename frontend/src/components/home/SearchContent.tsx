bbea86a3e4f9438f776c07a56f31e3e6c566c0c0
mariotc1 <mariotomecore@gmail.com>
2026-02-03 22:54:26 +0100

    clean code

--- frontend/src/components/home/SearchContent.tsx
+++ frontend/src/components/home/SearchContent.tsx
@@ -105,7 +105,6 @@ export default function SearchContent() {
           </div>
 
           {isLoading ? (
-            // SKELETON GRID - Replaces old spinner
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
               {[...Array(6)].map((_, i) => (
                 <CentroCardSkeleton key={`skeleton-${i}`} />
