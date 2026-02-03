084ee120003211b33e8d4804f756fafbd2eb5821
mariotc1 <mariotomecore@gmail.com>
2026-02-03 15:30:11 +0100

    clean code

--- backend/database/migrations/2026_01_24_102550_create_ciclo_fps_table.php
+++ backend/database/migrations/2026_01_24_102550_create_ciclo_fps_table.php
@@ -1,34 +1,27 @@
 <?php
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    return new class extends Migration {
 
-return new class extends Migration {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('ciclos_fp', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('centro_id')->constrained('centros')->onDelete('cascade');
-            $table->string('familia_profesional')->nullable();
-            $table->string('codigo_familia')->nullable();
-            $table->string('nivel_educativo')->nullable(); // Grado Medio, Superior
-            $table->string('clave_ciclo')->nullable();
-            $table->string('ciclo_formativo');
-            $table->string('modalidad')->nullable();
-            $table->string('tipo_ensenanza')->nullable();
-            $table->timestamps();
-        });
-    }
+        public function up(): void {
+            Schema::create('ciclos_fp', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('centro_id')->constrained('centros')->onDelete('cascade');
+                $table->string('familia_profesional')->nullable();
+                $table->string('codigo_familia')->nullable();
+                $table->string('nivel_educativo')->nullable();
+                $table->string('clave_ciclo')->nullable();
+                $table->string('ciclo_formativo');
+                $table->string('modalidad')->nullable();
+                $table->string('tipo_ensenanza')->nullable();
+                $table->timestamps();
+            });
+        }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('ciclos_fp');
-    }
-};
+        public function down(): void {
+            Schema::dropIfExists('ciclos_fp');
+        }
+    };
+?>
