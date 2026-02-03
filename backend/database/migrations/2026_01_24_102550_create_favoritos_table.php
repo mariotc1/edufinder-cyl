9daf10b7591b21c807e1577dfe13208507c94a24
mariotc1 <mariotomecore@gmail.com>
2026-02-03 15:32:01 +0100

    clean code

--- backend/database/migrations/2026_01_24_102550_create_favoritos_table.php
+++ backend/database/migrations/2026_01_24_102550_create_favoritos_table.php
@@ -1,28 +1,21 @@
 <?php
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    return new class extends Migration {
 
-return new class extends Migration {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('favoritos', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('centro_id')->constrained('centros')->onDelete('cascade');
-            $table->timestamps();
-        });
-    }
+        public function up(): void {
+            Schema::create('favoritos', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('user_id')->constrained()->onDelete('cascade');
+                $table->foreignId('centro_id')->constrained('centros')->onDelete('cascade');
+                $table->timestamps();
+            });
+        }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('favoritos');
-    }
-};
+        public function down(): void {
+            Schema::dropIfExists('favoritos');
+        }
+    };
+?>
