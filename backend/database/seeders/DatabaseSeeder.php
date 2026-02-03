926d91c9680902b5358b06038ade19fcf621df0d
mariotc1 <mariotomecore@gmail.com>
2026-02-03 13:48:45 +0100

    clean code

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -1,30 +1,17 @@
 <?php
+    namespace Database\Seeders;
 
-namespace Database\Seeders;
+    use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+    use Illuminate\Database\Seeder;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
+    class DatabaseSeeder extends Seeder {
+        use WithoutModelEvents;
 
-class DatabaseSeeder extends Seeder
-{
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        $this->call([
-            CentrosSeeder::class,
-            CiclosFpSeeder::class,
-        ]);
+        public function run(): void {
+            $this->call([
+                CentrosSeeder::class,
+                CiclosFpSeeder::class,
+            ]);
+        }
     }
-}
+?>
